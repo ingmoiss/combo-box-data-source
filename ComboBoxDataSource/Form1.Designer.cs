5f880546f96ad5bfd6096a8c584984f6e92d448b
Moisés Navarro <moi55navacor@gmail.com>
2021-03-20 15:31:35 -0600

    Agregar archivos de proyecto.

---
+++ ComboBoxDataSource/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace ComboBoxDataSource
+{
+    partial class frmCombo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboPersona = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboPersona
+            // 
+            this.cboPersona.FormattingEnabled = true;
+            this.cboPersona.Location = new System.Drawing.Point(68, 37);
+            this.cboPersona.Name = "cboPersona";
+            this.cboPersona.Size = new System.Drawing.Size(121, 21);
+            this.cboPersona.TabIndex = 0;
+            // 
+            // frmCombo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.cboPersona);
+            this.Name = "frmCombo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Combo With Data Source ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPersona;
+    }
+}
+
